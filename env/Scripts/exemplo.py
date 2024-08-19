7271b7714990133a830b0f9afe1572d4b87e5e90
FelipeMedeiros99 <52361556+FelipeMedeiros99@users.noreply.github.com>
2024-08-18 21:01:51 -0300

    Delete env directory
    DELETANDO AMBIENTE VIRTUAL

--- env/Scripts/exemplo.py
+++
@@ -1,7 +0,0 @@
-#!C:\Users\USER\Documents\1FELIPE\programaçao\python\Automacao-cadastro-tambau\env\Scripts\python.exe
-# -*- coding: utf-8 -*-
-
-from pycep import PyCep
-
-cep1 = PyCep(59104210)
-print(cep1.dadosCep)
