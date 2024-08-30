7b2501903a5077f7ed7163be52074a5866858779
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-08-30 20:46:13 -0300

    melhorando filtro das datas de nascimentos

--- tools/filtros/filtro_datas_de_nascimento.py
+++ tools/filtros/filtro_datas_de_nascimento.py
@@ -1,15 +1,13 @@
 from re import compile, findall
 
 def filtro_datas_de_nascimento(txt):
-    regex_nascimento = r'(0[0-9]|[12][0-9]|3[01]).?(0[0-9]|1[0-2]).?(19[7-9]\d|20\d\d|\d{2})'
+    regex_nascimento = r'(0[0-9]|[12][0-9]|3[01])\D?(0[0-9]|1[0-2])\D?(19[7-9]\d|20\d\d)'
     padrao = compile(regex_nascimento)
     nascimentos = findall(padrao, txt)
     nascimentos_ajustados = []
 
     for nascimento in nascimentos: 
         nascimento = list(nascimento)
-        if len(nascimento[2]) == 2:
-            nascimento[2] = f'19{nascimento[2]}'
         nascimentos_ajustados.append('/'.join(nascimento))
     
     return nascimentos_ajustados
